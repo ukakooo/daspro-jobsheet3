e6c1fc4db57d2071047a27e37c1f430fc23a5dc0
Herconary Angga <herconaryangga15@gmail.com>
2024-09-11 19:07:30 +0700

    Update Siakad Java dengan setiap nilai memiliki bobot persen masing-masing dan kelas dilengkapi dengan program studinya

--- Siakad12.java
+++ Siakad12.java
@@ -7,25 +7,31 @@ public class Siakad12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String nama, nim;
-        char kelas;
+        String kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        double nilaiKuis, nilaiTugas, nilaiUTS, nilaiUAS, nilaiAkhir;
 
         System.out.println("Masukkan nama: ");
         nama = sc.nextLine();
         System.out.println("Masukkan NIM: ");
         nim = sc.nextLine();
         System.out.println("Masukkan kelas: ");
-        kelas = sc.nextLine().charAt(0);
+        kelas = sc.nextLine();
         System.out.println("Masukkan nomor absen: ");
         absen = sc.nextByte();
         System.out.println("Masukkan nilai kuis: ");
         nilaiKuis = sc.nextDouble();
+        nilaiKuis*= 0.2;
         System.out.println("Masukkan nilai tugas: ");
         nilaiTugas = sc.nextDouble();
-        System.out.println("Masukkan nilai ujian: ");
-        nilaiUjian = sc.nextDouble();
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+        nilaiTugas*= 0.15;
+        System.out.println("Masukkan nilai UTS: ");
+        nilaiUTS = sc.nextDouble();
+        nilaiUTS*= 0.3;
+        System.out.println("Masukkan nilai UAS: ");
+        nilaiUAS = sc.nextDouble();
+        nilaiUAS*= 0.35;
+        nilaiAkhir = nilaiKuis + nilaiTugas + nilaiUTS + nilaiUAS;
         System.out.println("Nama: " + nama + " NIM: " + nim);
         System.out.println("Kelas: " + kelas + " Absen: " + absen);
         System.out.println("Nilai Akhir: " + nilaiAkhir);
