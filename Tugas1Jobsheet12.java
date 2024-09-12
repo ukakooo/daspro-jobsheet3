5ae251d3fd4921f96253eb9883af7c6ef41d5bc3
Herconary Angga <herconaryangga15@gmail.com>
2024-09-12 11:48:55 +0700

    Revisi Tugas 1 Jobsheet

--- Tugas1Jobsheet12.java
+++ Tugas1Jobsheet12.java
@@ -5,16 +5,13 @@ import java.util.Scanner;
 public class Tugas1Jobsheet12 {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        //Variabel-variabel
-        boolean penggunaanListrik;
+        Scanner input = new Scanner(System.in); 
+        //Variabel-variabel        
         int tarifListrik;
-        int jmlPenggunaanListrik;
+        int jmlPenggunaanListrik;        
 
         //Deklarasi
-        System.out.print("Apakah penggunaan listrik melebihi 500kWh? (True/False): ");
-        penggunaanListrik = input.nextBoolean();
-        System.out.print("Masukkan jumlah penggunaan listrik: ");
+            System.out.print("Masukkan jumlah penggunaan listrik: ");
         jmlPenggunaanListrik = input.nextInt();
         System.out.print("Masukkan nilai tarif listrik per kWh: ");
         tarifListrik = input.nextInt();
@@ -22,15 +19,12 @@ public class Tugas1Jobsheet12 {
 
         //Pengoperasian
         double totalTagihanListrik = jmlPenggunaanListrik * tarifListrik;
+        boolean penggunaanListrik = jmlPenggunaanListrik >= 500;        
 
         //Hasil pengoperasian dan deklarasi
         System.out.println("Total Tagihan Listrik adalah: Rp." +totalTagihanListrik);
-        if (penggunaanListrik == true){
-        System.out.println("Dan Penggunaan listrik melebihi 500 kWh");
-        }
-        if (penggunaanListrik == false){
-        System.out.println("Dan Penggunaan listrik tidak melebihi 500 kWh");
-        }
+        System.out.println("Apakah jumlah penggunaan listrik melebihi 500? " +penggunaanListrik);
+       
          
         
                                             
--- Tugas2Jobsheet12.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-public class Tugas2Jobsheet12 {
-    public static void main(String[] args) {
-        
-    }
-    
-}
